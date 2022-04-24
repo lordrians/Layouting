7b32a225cd205f4bf066ccd1dac78ac15492e38c
lordrians <ferrianrp@gmail.com>
2022-04-24 13:56:08 +0700

    adjust size text & update movie data

--- app/src/main/java/com/example/layouting/dtMovie.kt
+++ app/src/main/java/com/example/layouting/dtMovie.kt
@@ -16,80 +16,80 @@ class dtMovie {
     )
 
     val listPoster = listOf(
-        R.drawable.poster,
-        R.drawable.poster,
-        R.drawable.poster,
-        R.drawable.poster,
-        R.drawable.poster,
-        R.drawable.poster,
-        R.drawable.poster,
-        R.drawable.poster,
-        R.drawable.poster,
-        R.drawable.poster
+        R.drawable.poster_batman_1,
+        R.drawable.poster_nwh_2,
+        R.drawable.poster_turningred_3,
+        R.drawable.poster_sonic2_4,
+        R.drawable.poster_yaksha_5,
+        R.drawable.poster_alloldknives_6,
+        R.drawable.poster_moonfall_7,
+        R.drawable.poster_theoutfit_8,
+        R.drawable.poster_war_world_9,
+        R.drawable.poster_encanto_10
     )
 
     val listReleaseDate = listOf(
-        "03/02/2022",
-        "11/24/2021",
-        "11/24/2021",
-        "11/24/2021",
-        "11/24/2021",
-        "11/24/2021",
-        "11/24/2021",
-        "11/24/2021",
-        "11/24/2021",
-        "11/24/2021"
+        "03/02/2022 (ID)",
+        "12/17/2021 (US)",
+        "03/10/2022 (CZ)",
+        "04/08/2022 (US)",
+        "04/08/2022 (KR)",
+        "04/08/2022 (GB)",
+        "02/04/2022 (US)",
+        "03/18/2022 (US)",
+        "12/22/2021 (US)",
+        "11/24/2021 (US)"
     )
 
     val listDuration = listOf(
+        "2h 56m",
+        "2h 28m",
+        "1h 40m",
+        "2h 2m",
+        "2h 5m",
         "1h 42m",
-        "1h 42m",
-        "1h 42m",
-        "1h 42m",
-        "1h 42m",
-        "1h 42m",
-        "1h 42m",
-        "1h 42m",
-        "1h 42m",
+        "2h 10m",
+        "1h 45m",
+        "1h 53m",
         "1h 42m"
     )
 
     val listRate = listOf(
+        "7,9",
+        "8,1",
+        "7,5",
         "7,7",
-        "7,7",
-        "7,7",
-        "7,7",
-        "7,7",
-        "7,7",
-        "7,7",
-        "7,7",
-        "7,7",
+        "6,2",
+        "6,1",
+        "6,5",
+        "7,2",
+        "6.0",
         "7,7",
     )
 
     val listDirector = listOf(
-        "Byron Howard",
-        "Byron Howard",
-        "Byron Howard",
-        "Byron Howard",
-        "Byron Howard",
-        "Byron Howard",
-        "Byron Howard",
-        "Byron Howard",
-        "Byron Howard",
+        "Matt Reeves",
+        "Jon Watts",
+        "Domee Shi",
+        "Jeff Fowler",
+        "Ahn Sang-hoon",
+        "Janus Metz Pedersen",
+        "Harald Kloser",
+        "Johnathan McClain",
+        "Conor Dowling",
         "Byron Howard"
     )
 
     val listGenre = listOf(
-        "Animation, Comedy, Family, Fantasy",
-        "Animation, Comedy, Family, Fantasy",
-        "Animation, Comedy, Family, Fantasy",
-        "Animation, Comedy, Family, Fantasy",
-        "Animation, Comedy, Family, Fantasy",
-        "Animation, Comedy, Family, Fantasy",
-        "Animation, Comedy, Family, Fantasy",
-        "Animation, Comedy, Family, Fantasy",
-        "Animation, Comedy, Family, Fantasy",
+        "Crime, Mystery, Thriller",
+        "Action, Adventure, Science Fiction",
+        "Animation, Family, Comedy, Fantasy",
+        "Action, Science Fiction, Comedy, Family",
+        "Action",
+        "Thriller, Action",
+        "Action, Adventure, Science Fiction",
+        "Drama, Thriller, Crime",
+        "Science Fiction, Action",
         "Animation, Comedy, Family, Fantasy"
     )
 
